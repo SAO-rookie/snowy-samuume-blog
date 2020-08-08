8678e7a18e11eeafc5c7390bd1f60e62958ab349
snowy <xueyuren109@gmail.com>
2020-08-09 00:32:54 +0800

    添加返回工具类

---
+++ src/main/java/com/snowy_samuume/tool/CommonConstants.java
@@ -0,0 +1,16 @@
+package com.snowy_samuume.tool;
+
+/**
+ * @author snowy
+ * @date 2020/8/9 0:31
+ */
+public interface CommonConstants {
+    /**
+     * 成功标记
+     */
+    Integer SUCCESS = 0;
+    /**
+     * 失败标记
+     */
+    Integer FAIL = 1;
+}
