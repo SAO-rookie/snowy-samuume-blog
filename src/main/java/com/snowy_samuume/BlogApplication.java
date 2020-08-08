da2ebb60fc47b24f9039117a130d6d69693a5d4d
snowy <xueyuren109@gmail.com>
2020-08-09 00:11:37 +0800

    搭好框架

---
+++ src/main/java/com/snowy_samuume/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.snowy_samuume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author snowy
+ * @date 2020/8/9 0:10
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
