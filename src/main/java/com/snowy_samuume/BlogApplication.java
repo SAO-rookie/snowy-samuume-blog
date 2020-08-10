5ec60f2859fb9c0cafb155d2b182f8bb0e837478
snowy <xueyuren109@gmail.com>
2020-08-10 13:58:58 +0800

    添加返回工具类2

--- src/main/java/com/snowy_samuume/BlogApplication.java
+++ src/main/java/com/snowy_samuume/BlogApplication.java
@@ -2,12 +2,14 @@ package com.snowy_samuume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author snowy
  * @date 2020/8/9 0:10
  */
 @SpringBootApplication
+@EnableSwagger2
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class,args);
---
+++ src/main/java/com/snowy_samuume/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.snowy_samuume.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author snowy
+ * @date 2020/8/9 0:40
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.snowy_samuume.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("RESTful APIs")
+                .description("http://127.0.0.1:8080")
+                .contact("long")
+                .version("1.0")
+                .build();
+    }
+
+}
